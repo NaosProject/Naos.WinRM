65d96f2e6511d79919119bce56d1be88da45ab35
Lawson Caudill <lawson@caudillclan.us>
2015-04-28 13:26:08 -0400

    Adding initial spike to test out remote shell execution from .NET (using information from: http://www.codeproject.com/Articles/773685/Enable-Remote-PowerShell-Execution-in-Csharp).

---
+++ Naos.WinRM.Contract.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Naos.StackTemplate.Contract.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Naos.StackTemplate.Contract.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2a4cb6e-e115-4d45-b715-4c42cda9c14d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Naos.WinRM.Contract/RemoteCommand.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RemoteCommand.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.WinRM.Contract
+{
+    /// <summary>
+    /// Representation of a command to run on a remote machine.
+    /// </summary>
+    public class RemoteCommand
+    {
+        /// <summary>
+        /// Gets or sets the name of the computer to run the command on.
+        /// </summary>
+        public string ComputerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the command text to execute.
+        /// </summary>
+        public string CommandText { get; set; }
+    }
+}
